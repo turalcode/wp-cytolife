e3de85d3a1a759afa4404ecc9e0d5bf41e511096
turalcode <turalcodechannel@gmail.com>
2026-01-22 16:05:13 +0300

    спикеры

--- page-event.php
+++ page-event.php
@@ -145,6 +145,42 @@
                 </div>
             </div>
             <!-- /single-event-organizer-content -->
+
+            <div class="product__share">
+                <div class="product__share-title">Поделиться:</div>
+
+                <div class="product__share-list">
+                    <a href="<?php echo esc_url(get_tg_share_url(get_permalink())); ?>" class="product__share-link">
+                        <svg class="icon-share">
+                            <use href="#icon-tg-share"></use>
+                        </svg>
+                    </a>
+
+                    <a href="<?php echo esc_url(get_wa_share_url(get_permalink())); ?>" class="product__share-link">
+                        <svg class="icon-share">
+                            <use href="#icon-wa-share"></use>
+                        </svg>
+                    </a>
+
+                    <a href="<?php echo esc_url(get_mail_share_url(get_permalink())); ?>" class="product__share-link">
+                        <svg class="icon-share">
+                            <use href="#icon-mail-share"></use>
+                        </svg>
+                    </a>
+
+                    <a href="<?php echo esc_url(get_vk_share_url(get_permalink())); ?>" class="product__share-link">
+                        <svg class="icon-share icon-share--vk">
+                            <use href="#icon-vk-share"></use>
+                        </svg>
+                    </a>
+
+                    <a href="#" class="product__share-link share-link-js">
+                        <svg class="icon-share">
+                            <use href="#icon-copy-share"></use>
+                        </svg>
+                    </a>
+                </div>
+            </div>
         </div>
         <!-- /container -->
     </section>
---
+++ page-speakers.php
@@ -0,0 +1,49 @@
+<?php defined('ABSPATH') || exit; ?>
+
+<?php get_header(); ?>
+
+<div class="speakers">
+    <section class="speakers-f-screen section">
+        <div class="container">
+            <h1>Наши спикеры</h1>
+
+            <div class="speakers-f-screen-content">
+                <div class="row">
+                    <div class="col-md-5">
+                        <div class="speakers-f-screen-photo">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/founder.jpg" alt="Токарчук Кирилл Вячеславович. Основатель компании LABORATORY CYTOLIFE.">
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <div class="speakers-f-screen-quote">
+                            <div class="pos-r">
+                                <p>Перед вами команда спикеров Laboratory CYTOLIFE — практикующие врачи, кандидаты наук, сертифицированные тренеры.</p>
+
+                                <p>Мы не верим в «голую теорию» — за каждым выступлением стоит реальный опыт, клинические кейсы и научный подход. Наши специалисты публикуют статьи, готовят методические материалы, участвуют в разработке протоколов и обучающих программ для врачей.</p>
+
+                                <p>Всё, чем мы делимся, проверено практикой и подтверждено наукой.</p>
+
+                                <div class="speakers-f-screen-author">
+                                    <span>Токарчук Кирилл Вячеславович</span>
+                                    <div>Основатель компании LABORATORY&nbsp;CYTOLIFE</div>
+                                </div>
+
+                                <div class="speakers-f-screen-backtick">
+                                    <svg class="icon icon-backtick">
+                                        <use href="#icon-backtick"></use>
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /speakers-f-screen-content -->
+        </div>
+        <!-- /container -->
+    </section>
+    <!-- /speakers -->
+</div>
+<!-- /speakers -->
+
+<?php get_footer(); ?>
